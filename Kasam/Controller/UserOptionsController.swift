1ac837c6e6351a4a586b94303c6b85e1ecd60d30
Vance Basilio <vance.basilio@gmail.com>
2019-10-27 21:43:30 -0400

    added useroptions popups

---
+++ Kasam/Controller/UserOptionsController.swift
@@ -0,0 +1,70 @@
+//
+//  EmbeddedViewController.swift
+//  SwiftEntryKitDemo
+//
+//  Created by Daniel Huri on 3/15/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class UserOptionsController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet weak var slidingHandle: UIView!
+    
+   
+    private let dataSource = ["Create a Kasam", "Log Out"]
+    
+    init() {
+        super.init(nibName: type(of: self).className, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Contacts"
+        slidingHandle.layer.cornerRadius = 3
+        slidingHandle.clipsToBounds = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "GenericCell")
+    }
+}
+
+// MARK: UITableViewDelegate, UITableViewDataSource
+
+extension UserOptionsController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GenericCell")!
+        cell.selectionStyle = .none
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            performSegue(withIdentifier: "goToCreateKasam", sender: nil)
+        } else if indexPath.row == 1 {
+            AppManager.shared.logoout()
+            LoginManager().logOut()
+        }
+    }
+}
+
+extension NSObject {
+    var className: String {
+        return String(describing: type(of: self))
+    }
+    
+    class var className: String {
+        return String(describing: self)
+    }
+}
